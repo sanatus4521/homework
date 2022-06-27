58ef438b82e7f7d8bc0d19a9b65ba28a045d447e
Ally <sanatus4521@gmail.com>
2022-06-27 16:43:08 +0800

    Initial version

---
+++ src/main/kotlin/TesterKotlin.kt
@@ -0,0 +1,25 @@
+import jdk.nashorn.internal.objects.NativeRegExp.test
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Please enter number of tickets: ")
+    val number = scanner.nextInt()
+    println("How many round-trip tickets:  ")
+    val roundTrip = scanner.nextInt()
+    val a = TesterKotlin(number, roundTrip)
+    a.print()
+}
+
+class TesterKotlin(val number:Int, val roundTrip:Int) {
+    fun money() :Int {
+        val money = ((number * 1000) + (roundTrip * 800))
+        return money
+    }
+    fun print() {
+        println("Total tickets: $number")
+        println("Round-trip tickets: $roundTrip")
+        println("One-way tickets: " + (number-roundTrip))
+        println("Total: ${money()}")
+    }
+}
