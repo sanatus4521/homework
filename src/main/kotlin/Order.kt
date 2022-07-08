d1dbc23816e18903038e5c50d313dabcdcdb7bbf
Ally <sanatus4521@gmail.com>
2022-07-08 14:49:50 +0800

    Order(2.0)

--- src/main/kotlin/Order.kt
+++ src/main/kotlin/Order.kt
@@ -3,67 +3,61 @@ import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
-
+    val map = mutableMapOf<String, Menu>()
     val meals = mutableListOf<Menu>()
-    FileReader("mcdonalds").buffered().lines().forEach() {
+    FileReader("mcdonalds").buffered().lines().forEach {
         val spilt = it.split(",")
-        val item = Menu(spilt[0],spilt[1], spilt[2], spilt[3])
+        val item = Menu(spilt[1], spilt[2], spilt[3])
+        map.put(spilt[0], item)
         meals.add(item)
     }
-    meals[0].printMenu()
-    meals[1].printMenu()
-    meals[2].printMenu()
-    meals[3].printMenu()
+    for (key in map.keys) {
+        println(key)
+        map.getValue(key).printMenu()
+    }
     println("Hi! Here is the menu.")
+
     var keep:String
-    var sum = 0
+    var totalPrice = 0
     do {
-        var perMeals = OrderAny()
-        sum = perMeals + sum
+        val pricePerMeal = orderAny(map)
+        totalPrice = pricePerMeal + totalPrice
         println("If You want to order something else, please enter 'Y'. Otherwise, please enter 'N'.")
         keep = scanner.next()
     } while (keep == "Y")
 
-
-    println("The total money of your meal is $sum .")
+    println("Thank you for coming!")
+    println("The total price of your meal is $totalPrice")
+    println("Please go to the next window to check out.")
 
 }
 
-
-fun OrderAny(): Int {
+fun orderAny(map: MutableMap<String, Menu>): Int {
     val scanner = Scanner(System.`in`)
     println("If you want to order something, please enter the meal number: ")
     var meal = scanner.next()
-    var moneyPerMeal:Int = 0
-    if ( meal == "001" ) {
-        println("How many meals do you want? Please enter: ")
-        val number = scanner.nextInt()
-        moneyPerMeal = number * 80
-        println("The rice is: $moneyPerMeal")
-    } else if ( meal == "002" ) {
-        println("How many meals do you want? Please enter: ")
-        val number = scanner.nextInt()
-        moneyPerMeal = number * 49
-        println("The  price is: $moneyPerMeal")
-    } else if ( meal == "003" ) {
-        println("How many meals do you want? Please enter: ")
-        val number = scanner.nextInt()
-        moneyPerMeal = number * 60
-        println("The price is: $moneyPerMeal")
-    } else if ( meal == "004" ) {
-        println("How many meals do you want? Please enter: ")
-        val number = scanner.nextInt()
-        moneyPerMeal = number * 80
-        println("The price is: $moneyPerMeal")
-    } else {
+    while (meal != "001" && meal != "002" && meal != "003" && meal != "004") {
         println("Received wrong number! Please try again.")
+        meal = scanner.next()
     }
-    return moneyPerMeal
+    var pricePerMeal = 0
+    for (key in map.keys) {
+        when(meal) {
+            key -> {
+                println("How many meals do you want? Please enter: ")
+                val number = scanner.nextInt()
+                pricePerMeal = number * map.getValue(key).price.toInt()
+                println("The price is: $pricePerMeal")
+            }
+            else -> {
+            }
+        }
+    }
+    return pricePerMeal
 }
 
-data class Menu (val id:String, val name:String, val calories:String, val price:String) {
+data class Menu (val name:String, val calories:String, val price:String) {
     fun printMenu() {
-        println(id)
         println("meal: $name")
         println("calories: $calories")
         println("price: $price")
